7d11ca816f109ce18db67bda073e387b6a1f2a69
Christopher-Henrico <95366630+ChristopherHenrico@users.noreply.github.com>
2023-01-07 13:01:44 +0000

    Updated the API Key

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export OPENAI_API_KEY="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"
+export OPENAI_API_KEY="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"
 python3 ai-text-to-image.py
